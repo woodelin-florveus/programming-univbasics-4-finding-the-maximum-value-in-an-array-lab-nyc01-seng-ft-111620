8e7d30e4fac72e3394cf2e2b354aee9e342ddce4
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 13:26:49 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -7,5 +7,12 @@
 
 def find_max_value(array)
   count = 0
-  max_value = -1 
+  max_value = -1
+
+  while count < array.length
+    if max_value < array[count]
+      max_value = array[count]
+    end
+    count += 1
+  end
 end
