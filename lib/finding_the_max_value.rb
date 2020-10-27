85730107f81780c87ed85fa2dfebfe1a2133283f
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 13:14:56 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,10 @@
 def find_max_value(array)
   # Add your solution here
   # takes in an array of integers and should return whichever integer in the array has the highest value
-  # array.max
-  count = 0
-  while count[array] do
-    count[array].max
-    count += 1
-  end
+  array.max
+  # count = 0
+  # while count[array] do
+  #   count[array].max
+  #   count += 1
+  # end
 end
